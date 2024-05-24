ca8f28faf0a7629f5213809d33aef51819a7512f
camilatav <camiiila.toj@gmail.com>
2024-05-24 10:16:39 -0300

    Atualizando
    Signed-off-by: camilatav <camiiila.toj@gmail.com>

--- src/list/operacoesBasicas/ListaTarefa.java
+++ src/list/operacoesBasicas/ListaTarefa.java
@@ -4,34 +4,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListaTarefa {
-    private List<Tarefas> tarefasList;
+    private List<Tarefas> tarefasList; //tarefasList é uma lista do tipo List<Tarefas>
 
-    public ListaTarefa() {
-        this.tarefasList = new ArrayList<>();//arraylist vazio
+    public ListaTarefa() {//construtor
+        this.tarefasList = new ArrayList<>();//arraylist vazio, para armazenar as tarefas
+        //uso do this por ser um construtor
+        //a instanciação da lista tarefasList dentro do construtor é pq SÓ DEVE HAVER UMA LISTA. Se vc colocasse
+        //na função adicionarTarefa, a lista seria recriada toda vez que vc adicionasse uma nova tarefa, perdendo as
+        //tarefas anteriores.
     }
 
     public void adicionarTarefa(String descricao){
-        tarefasList.add(new Tarefas(descricao));
-    }
+        tarefasList.add(new Tarefas(descricao)); //Cria uma nova instância de Tarefas com a descrição fornecida e a adiciona à tarefasList.
+    } // vc tem uma lista chamada tarefasList e nela vc vai adicionar objetos instanciados da classe Tarefas
+    //o parametro descricao será substituido por um argumento(a descrição da tarefa) qnd vc instanciar uma tarefa.
+    // Ex: ''tarefa 1''
 
     public void removerTarefa(String descricao){
-        List<Tarefas> tarefasParaRemover = new ArrayList<>();
+        List<Tarefas> tarefasParaRemover = new ArrayList<>(); // instanciação da lista com o new
         for(Tarefas t : tarefasList){
             if(t.getDescricao().equalsIgnoreCase(descricao)){
                 tarefasParaRemover.add(t); //para cada tarefa t em tarefasList que seja igual a descrição de t
                 //coloque em tarefasParaRemover, isso para que remova os duplicados.
             }
-        }
+        } //a lista tarefasParaRemover é instanciada na função removerTarefa ao invés de no construtor pq
+        //seu uso é temporário e só necessário ao chamar a função removerTarefa.
         tarefasList.removeAll(tarefasParaRemover);
     //removeAll pq vc pode acabar tendo elementos repetidos na lista
     }
 
     public int obterNumeroTotalTarefas(){
-        return tarefasList.size();
+        return tarefasList.size(); // vc usa return e não sysout pq retornando esse valor vc usa ele pelo chamador do metodo
     }
 
     public void obterDescricoesTarefas(){
-        System.out.println(tarefasList);
+        System.out.println(tarefasList); //uso de sysout e n return pq vc nao precisa usar esse valor dps, só imprimir
     }
 
     public static void main(String[] args) {
--- src/list/operacoesBasicas/Tarefas.java
+++ src/list/operacoesBasicas/Tarefas.java
@@ -1,14 +1,14 @@
 package list.operacoesBasicas;
 
 public class Tarefas {
-    private String descricao;
+    private String descricao; //private pra dps ser acessado pelo metodo publico get
 
-    public Tarefas(String descricao) {
+    public Tarefas(String descricao) { //construtor (mesmo nome da classe)
         this.descricao = descricao;
     }
 
     public String getDescricao() {
-        return descricao;
+        return descricao;//metodo get publico pra acessar a string private descricao
     }
 
     @Override //qnd pedirmos para imprimir um objeto do tipo Tarefas, vai aparecer isso aqui:
